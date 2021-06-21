ad2a6edbb234c923d38be9a8808a159be593af39
rogeriocventura <rogeriocventura@hotmail.com>
2021-06-21 19:10:35 -0300

    commit do service e do model

---
+++ FRONT/src/app/models/Medicos.ts
@@ -0,0 +1,8 @@
+export class Medicos{
+    _id ?: String;
+    nome ?: String;
+    crm ?: String;
+    especialidade ?: String;
+    createdAt ?: Date;
+    updatedAt ?: Date;
+}
---
+++ FRONT/src/app/services/medico.service.ts
@@ -0,0 +1,26 @@
+import { Medicos } from './../models/Medicos';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  
+export class MedicossService {
+
+    private baseURL = "http://localhost:3000/";
+  
+    constructor(private http: HttpClient) { }
+  
+    listar() : Observable<Medicos[]>{
+      return this.http.get<Medicos[]>(`${this.baseURL}medico/listar`);
+    }
+  
+    cadastrar(medico : Medicos) : Observable<Medicos>{
+      return this.http.post<Medicos>(`${this.baseURL}medico/cadastrar`, medico);
+    }
+  }
+  
